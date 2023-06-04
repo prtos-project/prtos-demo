fb55f38390337efade8eaf2a6e96fbeec4c3a0d4
18939117 <18939117@qq.com>
2023-06-04 09:03:37 +0800

    code format
    Signed-off-by: 18939117 <18939117@qq.com>

--- partition_linux/xal-partition2/xal-partition.c
+++ partition_linux/xal-partition2/xal-partition.c
@@ -18,47 +18,42 @@
 
 #define SPORT0_READER_NAME "portS1"
 
-#define PRINTF(fmt, ...) \
-    printf("[P%d] " fmt , PRTOS_PARTITION_SELF, ## __VA_ARGS__)
+#define PRINTF(fmt, ...)                                                       \
+  printf("[P%d] " fmt, PRTOS_PARTITION_SELF, ##__VA_ARGS__)
 
-int
-Proc1(void)
-{
-    prtos_s32_t ret;
-    prtos_s32_t sWriterDesc;
-    prtos_s32_t sReaderDesc;
-    prtos_u32_t flags;
-    prtos_u32_t sSeq = 0;
-	char sMessage[32];
+int Proc1(void) {
+  prtos_s32_t ret;
+  prtos_s32_t sWriterDesc;
+  prtos_s32_t sReaderDesc;
+  prtos_u32_t flags;
+  prtos_u32_t sSeq = 0;
+  char sMessage[32];
 
-    PRINTF("Port %s created\n", SPORT0_READER_NAME);
-	sReaderDesc = prtos_create_sampling_port(SPORT0_READER_NAME, 128, PRTOS_DESTINATION_PORT, 0);
-	if (sReaderDesc < 0)
-	{
-		PRINTF("Ports error %d\n", sReaderDesc);
-		return -1;
-	}
+  PRINTF("Port %s created\n", SPORT0_READER_NAME);
+  sReaderDesc = prtos_create_sampling_port(SPORT0_READER_NAME, 128,
+                                           PRTOS_DESTINATION_PORT, 0);
+  if (sReaderDesc < 0) {
+    PRINTF("Ports error %d\n", sReaderDesc);
+    return -1;
+  }
 
-	while (1)
-	{
-		memset(sMessage, 0, sizeof(sMessage));
-        ret=prtos_read_sampling_message(sReaderDesc, sMessage, sizeof(sMessage), &flags);
-		PRINTF("Port%d read message [%s] %d\n", sReaderDesc, sMessage, ret);
-        if (ret > 0)
-        {
-        }
-    	prtos_idle_self();
+  while (1) {
+    memset(sMessage, 0, sizeof(sMessage));
+    ret = prtos_read_sampling_message(sReaderDesc, sMessage, sizeof(sMessage),
+                                      &flags);
+    PRINTF("Port%d read message [%s] %d\n", sReaderDesc, sMessage, ret);
+    if (ret > 0) {
     }
+    prtos_idle_self();
+  }
 
-    return 0;
+  return 0;
 }
 
-void
-PartitionMain(void)
-{
-    PRINTF("Receiver Partition.\n");
+void PartitionMain(void) {
+  PRINTF("Receiver Partition.\n");
 
-	PRINTF("Initialisation completed\n");
+  PRINTF("Initialisation completed\n");
 
-	Proc1();
+  Proc1();
 }

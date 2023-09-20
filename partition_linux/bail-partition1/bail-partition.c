4cf55a2f689bb7f65fbcd22770cf737e82d65f65
18939117 <18939117@qq.com>
2023-09-20 08:02:45 -0400

    Update partition linux
    Signed-off-by: 18939117 <18939117@qq.com>

--- partition_linux/bail-partition1/bail-partition.c
+++ partition_linux/bail-partition1/bail-partition.c
@@ -1,11 +1,5 @@
 /*
- * $FILE: xal-partition
- *
- * Event example
- *
- * $VERSION$
- *
- * $AUTHOR
+ * $FILE: bail-partition
  *
  */
 
@@ -16,7 +10,8 @@
 
 #include <prtos.h>
 
-#define SPORT0_READER_NAME "portS1"
+#define SPORT1_WRITER_NAME "portS1"
+#define SPORT0_READER_NAME "portS0"
 
 #define PRINTF(fmt, ...)                                                       \
   printf("[P%d] " fmt, PRTOS_PARTITION_SELF, ##__VA_ARGS__)
@@ -29,21 +24,36 @@ int Proc1(void) {
   prtos_u32_t sSeq = 0;
   char sMessage[32];
 
-  PRINTF("Port %s created\n", SPORT0_READER_NAME);
   sReaderDesc = prtos_create_sampling_port(SPORT0_READER_NAME, 128,
                                            PRTOS_DESTINATION_PORT, 0);
+  PRINTF("Port %s created\n", SPORT0_READER_NAME);
   if (sReaderDesc < 0) {
-    PRINTF("Ports error %d\n", sReaderDesc);
+    PRINTF("Port %s error %d\n", SPORT0_READER_NAME, sReaderDesc);
+    return -1;
+  }
+
+  PRINTF("Port %s created\n", SPORT1_WRITER_NAME);
+  sWriterDesc =
+      prtos_create_sampling_port(SPORT1_WRITER_NAME, 128, PRTOS_SOURCE_PORT, 0);
+  if (sWriterDesc < 0) {
+    PRINTF("Port %s error %d\n", SPORT1_WRITER_NAME, sWriterDesc);
     return -1;
   }
 
   while (1) {
-    memset(sMessage, 0, sizeof(sMessage));
     ret = prtos_read_sampling_message(sReaderDesc, sMessage, sizeof(sMessage),
                                       &flags);
-    PRINTF("Port%d read message [%s] %d\n", sReaderDesc, sMessage, ret);
+
     if (ret > 0) {
+      PRINTF("Port%d read message [%s] %d\n", sReaderDesc, sMessage, ret);
+    } else {
+      memset(sMessage, 0, sizeof(sMessage));
+      sprintf(sMessage, "message payload%d", sSeq++);
     }
+
+    ret = prtos_write_sampling_message(sWriterDesc, sMessage, sizeof(sMessage));
+    PRINTF("Port%d write message [%s] %d\n", sWriterDesc, sMessage, ret);
+
     prtos_idle_self();
   }
 
@@ -51,7 +61,7 @@ int Proc1(void) {
 }
 
 void partition_main(void) {
-  PRINTF("Receiver Partition.\n");
+  PRINTF("Sender Partition.\n");
 
   PRINTF("Initialisation completed\n");
 
--- partition_linux/bail-partition2/bail-partition.c
+++ partition_linux/bail-partition2/bail-partition.c
@@ -1,8 +1,5 @@
 /*
- * $FILE: xal-partition
- *
- * Event example
- *
+ * $FILE: bail-partition
  *
  */
 
@@ -13,8 +10,7 @@
 
 #include <prtos.h>
 
-#define SPORT1_WRITER_NAME "portS1"
-#define SPORT0_READER_NAME "portS0"
+#define SPORT0_READER_NAME "portS1"
 
 #define PRINTF(fmt, ...)                                                       \
   printf("[P%d] " fmt, PRTOS_PARTITION_SELF, ##__VA_ARGS__)
@@ -27,36 +23,21 @@ int Proc1(void) {
   prtos_u32_t sSeq = 0;
   char sMessage[32];
 
+  PRINTF("Port %s created\n", SPORT0_READER_NAME);
   sReaderDesc = prtos_create_sampling_port(SPORT0_READER_NAME, 128,
                                            PRTOS_DESTINATION_PORT, 0);
-  PRINTF("Port %s created\n", SPORT0_READER_NAME);
   if (sReaderDesc < 0) {
-    PRINTF("Port %s error %d\n", SPORT0_READER_NAME, sReaderDesc);
-    return -1;
-  }
-
-  PRINTF("Port %s created\n", SPORT1_WRITER_NAME);
-  sWriterDesc =
-      prtos_create_sampling_port(SPORT1_WRITER_NAME, 128, PRTOS_SOURCE_PORT, 0);
-  if (sWriterDesc < 0) {
-    PRINTF("Port %s error %d\n", SPORT1_WRITER_NAME, sWriterDesc);
+    PRINTF("Ports error %d\n", sReaderDesc);
     return -1;
   }
 
   while (1) {
+    memset(sMessage, 0, sizeof(sMessage));
     ret = prtos_read_sampling_message(sReaderDesc, sMessage, sizeof(sMessage),
                                       &flags);
-
+    PRINTF("Port%d read message [%s] %d\n", sReaderDesc, sMessage, ret);
     if (ret > 0) {
-      PRINTF("Port%d read message [%s] %d\n", sReaderDesc, sMessage, ret);
-    } else {
-      memset(sMessage, 0, sizeof(sMessage));
-      sprintf(sMessage, "message payload%d", sSeq++);
     }
-
-    ret = prtos_write_sampling_message(sWriterDesc, sMessage, sizeof(sMessage));
-    PRINTF("Port%d write message [%s] %d\n", sWriterDesc, sMessage, ret);
-
     prtos_idle_self();
   }
 
@@ -64,7 +45,7 @@ int Proc1(void) {
 }
 
 void partition_main(void) {
-  PRINTF("Sender Partition.\n");
+  PRINTF("Receiver Partition.\n");
 
   PRINTF("Initialisation completed\n");
 
